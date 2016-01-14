88b3d87212957a53be5270a8368a660528a55db7
Brady <bpotaczek@yahoo.com>
2016-01-14 11:27:30 -0600

    Update example.js

--- examples/example.js
+++ examples/example.js
@@ -1,6 +1,7 @@
 var winston = require('winston');
-var slack = require('./utils/winston-slack-web').Slack;
+var slack = require('winston-slack-web').Slack;
 
+// adding the transport
 winston.add(winston.transports.Slack, { 
     url: 'https://hooks.slack.com/services/[mykey]', 
     level: 'error',
@@ -8,3 +9,15 @@ winston.add(winston.transports.Slack, {
     displayname: 'My App Error Bot',
     displayicon: 'https://slack.com/img/icons/app-57.png'
 });
+winston.error('My error message');
+
+// or using an instance
+var logger = new winston.Logger({
+    transports: [
+        new (winston.transports.Slack)({
+            level: 'info',
+            url: 'https://hooks.slack.com/services/[mykey]'
+        })
+    ]
+});
+logger.info('Informational message');
